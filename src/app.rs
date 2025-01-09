df7e319b62c8de6af0a6e2a609f93951f69f28a4
JCodV <154752793+JCodV@users.noreply.github.com>
2025-01-08 23:17:20 -0500

    added sample code from a ratatui example for testing, will be removed later

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,10 @@
-use std::collections::HashMap;
-
 use color_eyre::{owo_colors::OwoColorize, Result};
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use ratatui::{
+    layout::Constraint,
     style::{Color, Style, Stylize},
     text::Line,
-    widgets::{Block, Paragraph},
+    widgets::{Block, Paragraph, Row, Table},
     DefaultTerminal, Frame,
 };
 
@@ -116,7 +115,8 @@ impl App {
     fn save_projects(file_name: &str) {}
 
     fn draw(&mut self, frame: &mut Frame) {
-        self.draw_project_selection_page(frame);
+        //self.draw_project_selection_page(frame);
+        self.draw_test(frame);
     }
 
     fn draw_project_selection_page(&self, frame: &mut Frame) {
@@ -125,12 +125,56 @@ impl App {
         let text = "Hello, Ratatui!\n\n\
             Created using https://github.com/ratatui/templates\n\
             Press `Esc`, `Ctrl-C` or `q` to stop running.";
+
+        let rows = [Row::new(vec!["hello", "this", "is", "a", "test"])];
+        let table_widths = [
+            Constraint::Length(5),
+            Constraint::Length(5),
+            Constraint::Length(10),
+        ];
+
+        let table = Table::new(rows, table_widths);
+
         frame.render_widget(
             Paragraph::new(text)
                 .block(Block::bordered().title(title))
                 .centered(),
             frame.area(),
-        )
+        );
+    }
+
+    fn draw_test(&self, frame: &mut Frame) {
+        let rows = [Row::new(vec!["Cell1", "Cell2", "Cell3"])];
+        // Columns widths are constrained in the same way as Layout...
+        let widths = [
+            Constraint::Length(5),
+            Constraint::Length(5),
+            Constraint::Length(10),
+        ];
+        let table = Table::new(rows, widths)
+            // ...and they can be separated by a fixed spacing.
+            .column_spacing(1)
+            // You can set the style of the entire Table.
+            .style(Style::new().blue())
+            // It has an optional header, which is simply a Row always visible at the top.
+            .header(
+                Row::new(vec!["Col1", "Col2", "Col3"])
+                    .style(Style::new().bold())
+                    // To add space between the header and the rest of the rows, specify the margin
+                    .bottom_margin(1),
+            )
+            // It has an optional footer, which is simply a Row always visible at the bottom.
+            .footer(Row::new(vec!["Updated on Dec 28"]))
+            // As any other widget, a Table can be wrapped in a Block.
+            .block(Block::new().title("Table"))
+            // The selected row, column, cell and its content can also be styled.
+            .row_highlight_style(Style::new().reversed())
+            .column_highlight_style(Style::new().red())
+            .cell_highlight_style(Style::new().blue())
+            // ...and potentially show a symbol in front of the selection.
+            .highlight_symbol(">>");
+
+        frame.render_widget(table, frame.area());
     }
 
     fn draw_project_creation_popup(&self, frame: &mut Frame) {}
